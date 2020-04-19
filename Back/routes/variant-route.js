08131d8fc5a5f72e289c796b0404f504dd87fcd9
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-04-20 02:22:00 +0300

    (20.04.2020)(Back)(get ready variant request)

--- Back/routes/variant-route.js
+++ Back/routes/variant-route.js
@@ -1,17 +1,64 @@
-const variantConfig = require("../config/data/variant-data.js");
-
 module.exports = function(server, database){
 
+	 function getVariant(variant, exercisesCollection){
+		return new Promise(async (resolve, reject)=>{
+			let response = [];
+			for (var i = 0; i < variant.exercisesIDs.length; i++) {
+				await getExercise(parseInt(variant.exercisesIDs[i]), exercisesCollection).then(
+					resolve=>{
+						response[i] = resolve;
+					},
+					error=>{
+						console.log(error);
+					}
+				)
+			}
+			resolve(response);
+		});
+	}
+
+	function getExercise(id, exercisesCollection){
+		return new Promise((resolve, reject)=>{
+			exercisesCollection.findOne({"_id":id}, (err, result)=>{
+				if(err){
+					reject(err);
+				}
+				resolve(result);
+			});
+		});
+	}
+
 	server.get("/:subject/variant.getCount/", (req, res)=>{
-	let parameter = "variants."+req.params.subject;
-	const collection = database.collection(parameter.toString());
-  	collection.countDocuments({}, (err, count)=>{
-  		console.log(count);
-  		res.json({'count':33});
-  		});
+		let parameter = "variants."+req.params.subject;
+		const collection = database.collection(parameter.toString());
+	  collection.countDocuments({}, (err, count)=>{
+			if(err){
+				throw err;
+			}
+	  		res.json({'count':count});
+	  	});
 	});
 
 	server.get("/:subject/variant.getReady/:id", (req, res)=>{
+
+			let subject = String(req.params.subject);
+			let varId = parseInt(req.params.id);
+			const varCollection = database.collection("variants."+subject);
+			const exercisesCollection = database.collection("exercises."+subject);
+			varCollection.findOne({"_id":varId}, (err, variant)=>{
+				if(err){
+					throw err;
+				}
+					console.log(variant.exercisesIDs);
+					getVariant(variant, exercisesCollection).then(
+							resolve=>{
+								res.json(resolve);
+							},
+							error=>{
+								console.log(error)
+							}
+					)
+			});
 
 	});
 
--- Back/server.js
+++ Back/server.js
@@ -15,10 +15,17 @@ server.use(cors(corsOptions));
 mongodb.connect(db.url, function(err, client){
   if(err){
     console.log(err);
+  }else {
+    const database = client.db("example");
+
+    server.use((req, res, next)=>{
+      let date = new Date();
+      console.log("Time: " + date + " Req: " + req.path);
+      next();
+    });
+
+    require("./routes/routes.js")(server, database);
   }
-  	const database = client.db("example");
-  	
-  	require("./routes/routes.js")(server, database);
 });
 
 
